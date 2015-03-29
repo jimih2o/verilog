09458ab4f58fae30f14928e50666ab9eef203281
JimiH <jimi.h.awesome@gmail.com>
2015-03-29 14:30:31 -0700

    ns
    synch

--- fixedpointexpo/fixed_exp.v
+++ fixedpointexpo/fixed_exp.v
@@ -3,25 +3,25 @@ input [2 : -7] x ;
 output [4 : -5] r ;
 input clk, rst ;
 
-reg [2:-31] rem = 34'b0 ;
+reg [2:-29] rem = 32'b0 ;
 reg [4:-5] exp = 10'b0000100000 ;
 
 assign r = exp ;
 
-function [2:-31] logn ;
+function [2:-29] logn ;
 	input [3:0] n ;
 
 	begin : LogN_Table
 		case (n)
-		4'd0: logn = 34'b0010100111000000010000100000000000 ;
-		4'd1: logn = 34'b0010000110010011111010101000000000 ;
-		4'd2: logn = 34'b0000101100010111001000011000000000 ;
-		4'd3: logn = 34'b0000011001111100110010001111100000 ;
-		4'd4: logn = 34'b0000001110010001111111101111100000 ;
-		4'd5: logn = 34'b0000000111100010011100000111011000 ;
-		4'd6: logn = 34'b0000000011111000010100011000011000 ;
-		4'd7: logn = 34'b0000000001111110000010100110110000 ;
-		4'd8: logn = 34'b0000000000111111100000010101000110 ;
+		4'd0: logn = 32'b00110011100000001000010000000000 ;
+		4'd1: logn = 32'b00100011001001111101010100000000 ;
+		4'd2: logn = 32'b00010110001011100100001100000000 ;
+		4'd3: logn = 32'b00001100111110011001000111110000 ;
+		4'd4: logn = 32'b00000111001000111111110111110000 ;
+		4'd5: logn = 32'b00000011110001001110000011101100 ;
+		4'd6: logn = 32'b00000001111100001010001100001100 ;
+		4'd7: logn = 32'b00000000111111000001010011011000 ;
+		4'd8: logn = 32'b00000000011111110000001010100011 ;
 		default: logn = 0 ;
 		endcase
 	end
@@ -47,97 +47,106 @@ function [4:-5] factor ;
 	end
 endfunction
 
-reg [3:0] i = 0 ;
+/*
+reg [3:0] i = 4'b0 ;
+reg [4:-5] e = 10'b0 ;
+*/
 
 always @(posedge clk or posedge rst) begin
 	if (rst) begin
-                exp <= 10'b0000100000 ;
-                rem <= x ; 
-                i <= 0 ;
+                exp = 10'b0000100000 ;
+                rem = x << 22 ;
+//                i <= 0 ;
         end else begin : Calculate_Block
-/*                reg [3:0] i ;
+                reg [3:0] i ;
                 reg [4:-5] e ;
                 
-                rem <= (x << 24) ;
-                e <= 10'b0000100000 ;
+                rem = (x << 22) ;
+                e = 10'b0000100000 ;
                 
                 for (i = 4'b0; i < 4'd9; i = i + 4'b1) begin
                         if (logn(i) < rem) begin
-                                rem <= rem - logn(i) ;
-                                e <= factor(i, e) ;
+                                rem = rem - logn(i) ;
+                                e = factor(i, e) ;
                         end
                 end
                 
-                exp <= e ;
+                exp = e ;
         end
-*/
+        
+//                $display("e = %5.5f, x = %3.7f, rem = %3.7f, i = %d", e / $itor(1 << 5), x / $itor(1 << 7), rem / $itor(1 << 31), i) ;
+/*
+                i <= i + 4'b1 ;
                 case (i)
                 4'd0: begin
                         // initialization:
                         // e = 1
-                        exp <= 10'b10000000000 ;
-                        rem <= x << 24 ;
+                        e = 10'b0000100000 ;
+                        // x is 3.7 => rem 3.29
+                        // x -> rem = << (29 - 7) = << 22
+                        rem = x << 22 ;
                         
-                        // condition
-                        if (logn(i) < rem) begin
+                        if (rem >= logn(i)) begin
                                 rem = rem - logn(i) ;
-                                exp = exp * (factor(i) << 5) ;
+                                e = e + (e << 10'd2) ;
                         end
                 end
                 4'd1: begin
-                        if (logn(i) < rem) begin
-                                rem = rem - logn(i) ;
-                                exp = exp * (factor(i) << 5) ;
+                        if (rem >= logn(i)) begin
+                                rem <= rem - logn(i) ;
+                                e <= e + (e << 10'd1) ;
                         end
                 end
                 4'd2: begin
-                        if (logn(i) < rem) begin
-                                rem = rem - logn(i) ;
-                                exp = exp * (factor(i) << 5) ;
+                        if (rem >= logn(i)) begin
+                                rem <= rem - logn(i) ;
+                                e <= (e << 10'd1) ;
                         end
                 end
                 4'd3: begin
-                        if (logn(i) < rem) begin
-                                rem = rem - logn(i) ;
-                                exp = exp * (factor(i) << 5) ;
+                        if (rem >= logn(i)) begin
+                                rem <= rem - logn(i) ;
+                                e <= e + (e >> 10'd1) ;
                         end
                 end
                 4'd4: begin
-                        if (logn(i) < rem) begin
-                                rem = rem - logn(i) ;
-                                exp = exp * (factor(i) << 5) ;
+                        if (rem >= logn(i)) begin
+                                rem <= rem - logn(i) ;
+                                e <= e + (e >> 10'd2) ;
                         end
                 end
                 4'd5: begin
-                        if (logn(i) < rem) begin
-                                rem = rem - logn(i) ;
-                                exp = exp * (factor(i) << 5) ;
+                        if (rem >= logn(i)) begin
+                                rem <= rem - logn(i) ;
+                                e <= e + (e >> 10'd3) ;
                         end
                 end
                 4'd6: begin
-                        if (logn(i) < rem) begin
-                                rem = rem - logn(i) ;
-                                exp = exp * (factor(i) << 5) ;
+                        if (rem >= logn(i)) begin
+                                rem <= rem - logn(i) ;
+                                e <= e + (e >> 10'd4) ;
                         end
                 end
                 4'd7: begin
-                        if (logn(i) < rem) begin
-                                rem = rem - logn(i) ;
-                                exp = exp * (factor(i) << 5) ;
+                        if (rem >= logn(i)) begin
+                                rem <= rem - logn(i) ;
+                                e <= e + (e >> 10'd5) ;
                         end
                 end
                 4'd8: begin
-                        if (logn(i) < rem) begin
-                                rem = rem - logn(i) ;
-                                exp = exp * (factor(i) << 5) ;
+                        if (rem >= logn(i)) begin
+                                rem <= rem - logn(i) ;
+                                e <= e + (e >> 10'd6) ;
                         end
                 end
                 4'd9: begin
-                        val = 
+                        exp <= e ;
+                        i <= 4'b0 ;
                 end
-                default: i = 9 ;
+                default: i <= 4'b0 ;
                 endcase
         end
+*/
 end
 
 endmodule 
---
+++ fixedpointexpo/output_files/hwexpo.cdf
@@ -0,0 +1,13 @@
+/* Quartus II 64-Bit Version 13.0.0 Build 156 04/24/2013 SJ Web Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP2C20F484) Path("C:/Users/jimi/Documents/GitHub/verilog/fixedpointexpo/output_files/") File("hwexpo.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
